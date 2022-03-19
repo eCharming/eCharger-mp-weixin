e9eec30855d586f59b951d6aae194b2ead63a802
D-Sketon <2055272094@qq.com>
2022-03-19 20:19:45 +0800

    v1.0.12.1主页修改

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,40 +1,39 @@
 <template>
 	<view>
 		<mymap></mymap>
-		<movebox ref="movebox">
-			<card>
-				<totalbutton :text1="'租电桩'" :text2="'电桩共享'" :selected="isSelected1" @tap="tap1()" ></totalbutton>
-				<totalbutton :text1="'借电桩'" :text2="'出租电桩'" :selected="isSelected2" @tap="tap2()" ></totalbutton>
-			</card>
+		<movebox>
+			<template slot="locationBtn">
+				<locationbutton />
+				</locationbutton>
+			</template>
+			<template slot="card">
+				<card>
+					<totalbutton :text1="'租电桩'" :text2="'电桩共享'" :selected="isSelected1" @tap="tap1()"></totalbutton>
+					<totalbutton :text1="'借电桩'" :text2="'出租电桩'" :selected="isSelected2" @tap="tap2()"></totalbutton>
+				</card>
+			</template>
 		</movebox>
 	</view>
 </template>
 
 <script>
-	import babtouchbox from '@/components/bab-Touchbox.vue'
 	import totalbutton from '@/components/totalbutton.vue'
 	import movebox from '@/components/moveBox.vue'
 	import card from '@/components/card.vue'
-	import mymap from '../../components/myMap.vue'
-
+	import mymap from '@/components/myMap.vue'
+	import locationbutton from '@/components/locationButton.vue'
 	export default {
 		components: {
-			babtouchbox,
 			totalbutton,
 			movebox,
 			mymap,
-			card
+			card,
+			locationbutton
 		},
 		data() {
 			return {
 				isSelected1: true,
 				isSelected2: false,
-			}
-		},
-		computed: {
-			test() {
-				console.log(this.$refs.movebox);
-				return this.$ref;
 			}
 		},
 		methods: {
