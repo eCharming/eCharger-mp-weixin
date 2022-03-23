5a2f6561b1f334a8349481a3d3ca799272d96143
D-Sketon <2055272094@qq.com>
2022-03-23 21:18:18 +0800

    v 1.1.8 删除无用文件

--- App.vue
+++ App.vue
@@ -1,8 +1,3 @@
-<style lang="scss">
-	/* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
-	@import "@/uview-ui/index.scss";
-</style>
-
 <script>
 	export default {
 		onLaunch: function() {
@@ -18,10 +13,8 @@
 </script>
 
 <style>
+	/*每个页面公共css */
 	@import "colorui/main.css";
 	@import "colorui/icon.css";
-	
-
-	/*每个页面公共css */
 </style>
 
