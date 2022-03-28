82c5c6c803a995777c42b00e956f58e4a955059a
D-Sketon <2055272094@qq.com>
2022-03-28 17:00:17 +0800

    v 1.2.4 地图重大更新，修复重大BUG,可能会有新BUG

--- components/myMap.vue
+++ components/myMap.vue
@@ -1,7 +1,8 @@
 <template>
-	<view >
-		<map id="myMap" ref="map" style="width: 100%; height: 100vh;" :latitude="latitude" :longitude="longitude"
-			:markers="covers" :setting="mapSetting" :circles="circles" :scale="scale" @markertap="markertap($event)">
+	<view>
+		<map id="myMap" ref="map" style="width: 100%; height: 100vh;" :latitude="center_latitude"
+			:longitude="center_longitude" :markers="covers" :setting="mapSetting" :circles="circles" :scale="scale"
+			@markertap="markertap($event)">
 		</map>
 	</view>
 </template>
@@ -11,8 +12,10 @@
 	export default {
 		data() {
 			return {
-				latitude: 39.909,
-				longitude: 116.39742,
+				center_latitude: 39.909, //中心纬度
+				center_longitude: 116.39742, //中心经度
+				latitude: 39.909, //当前纬度
+				longitude: 116.39742, //当前经度
 				mapSetting: {
 					"showCompass": true,
 					"enableOverlooking": false,
@@ -28,13 +31,13 @@
 				circles: [],
 				mapContext: null,
 				scale: 16,
-				ordersCopy:[],
-				url:'https://apis.map.qq.com/ws/distance/v1/matrix/?mode=driving&from=',
-				distance:[],
-				markerSelected:-1,
+				ordersCopy: [],
+				url: 'https://apis.map.qq.com/ws/distance/v1/matrix/?mode=driving&from=',
+				distance: [],
+				markerSelected: -1,
 			}
 		},
-		mounted() {
+		mounted() { 
 			this.mapContext = uni.createMapContext('myMap', this);
 			this.openLocation();
 		},
@@ -44,7 +47,7 @@
 				this.mapContext.moveToLocation({
 					latitude: lat,
 					longitude: lon,
-					fail: e => {	//fail后500ms重新执行
+					fail: e => { //fail后500ms重新执行
 						setTimeout(function() {
 							_this.MoveLocation(lat, lon)
 						}, 500)
@@ -64,16 +67,14 @@
 						let chargerList = res.result.data;
 						this.ordersCopy.splice(0);
 						this.covers.splice(0, this.covers.length);
-						this.markerSelected=-1;
-						if (chargerList.length!=0) {
+						this.markerSelected = -1;
+						if (chargerList.length != 0) {
 							for (let charger of chargerList) {
 								this.covers.push({
 									title: charger.location,
 									id: charger._id,
-									latitude: charger.geoPoint.coordinates[
-										1],
-									longitude: charger.geoPoint
-										.coordinates[0],
+									latitude: charger.geoPoint.coordinates[1],
+									longitude: charger.geoPoint.coordinates[0],
 									iconPath: "/static/image/charger.png",
 									width: 40,
 									height: 40,
@@ -87,22 +88,23 @@
 										padding: 10,
 									}
 								});
-								var distance=(this.getFlatternDistance(lat,lon,charger.geoPoint.coordinates[1],charger.geoPoint.coordinates[0])/1000).toFixed(1);
-								this.ordersCopy.push({        //为解决每次插入后界面都刷新使用了副本记录 最后一次性赋给store中的order
+								var distance = (this.getFlatternDistance(lat, lon, charger.geoPoint.coordinates[1],
+									charger.geoPoint.coordinates[0]) / 1000).toFixed(1);
+								this.ordersCopy.push({ //为解决每次插入后界面都刷新使用了副本记录 最后一次性赋给store中的order
 									location: charger.location,
 									id: charger._id,
 									latitude: charger.geoPoint.coordinates[1],
 									longitude: charger.geoPoint.coordinates[0],
-									price:charger.price,
-									startTime:'08:00',
-									endTime:'18:00',
-									distance:distance,
-									detail:false,
+									price: charger.price,
+									startTime: '08:00',
+									endTime: '18:00',
+									distance: distance,
+									detail: false,
 								});
 							}
 						}
-						this.$store.commit('setOrders',this.ordersCopy);
-						if (tle) {	//为了防止异步问题，所以放在这里
+						this.$store.commit('setOrders', this.ordersCopy);
+						if (tle) { //为了防止异步问题，所以放在这里
 							this.covers.push({
 								title: tle,
 								id: 278,
@@ -121,7 +123,7 @@
 									padding: 10,
 								}
 							});
-							this.circles.splice(1,1,{
+							this.circles.splice(0, 1, {
 								latitude: lat,
 								longitude: lon,
 								fillColor: "#4162996A",
@@ -137,17 +139,21 @@
 				wx.startLocationUpdate({
 					success: (res) => {
 						let oldlatitude = null;
-						let oldlongtitude = null;
+						let oldlongitude = null;
 						let firstFlag = true;
 						wx.onLocationChange((res) => { //调用实时获取定位
 							oldlatitude = this.latitude;
-							oldlongtitude = this.longitude;
+							oldlongitude = this.longitude;
 							this.latitude = res.latitude;
 							this.longitude = res.longitude;
-							this.$store.commit('setCurrentLocation',{
-								latitude:this.latitude,
-								longitude:this.longitude
+							this.$store.commit('setCurrentLocation', {
+								latitude: this.latitude,
+								longitude: this.longitude
 							});
+							if(firstFlag) {
+								this.center_latitude=this.latitude;
+								this.center_longitude=this.longitude;
+							}
 							this.circles.splice(0, 1, {
 								latitude: this.latitude,
 								longitude: this.longitude,
@@ -156,12 +162,12 @@
 								radius: 300,
 								strokeWidth: 1,
 							})
-							if (firstFlag || (oldlatitude && oldlongtitude && Math.abs(
-									oldlatitude - this.latitude) + Math.abs(oldlongtitude - this
+							if (firstFlag || (oldlatitude && oldlongitude && Math.abs(
+									oldlatitude - this.latitude) + Math.abs(oldlongitude - this
 									.longitude) > 0.005)) { //两次定位距离过近时不调用云函数以减小负载
 								this.getChargerLocation(this.longitude, this.latitude, null);
 							}
-							firstFlag = false;
+							firstFlag=false;
 						});
 					},
 					fail: (err) => {
@@ -170,62 +176,62 @@
 					}
 				})
 			},
-			getFlatternDistance(lat1,lng1,lat2,lng2){
-				
+			getFlatternDistance(lat1, lng1, lat2, lng2) {
+
 				var EARTH_RADIUS = 6378137.0; //单位M
 				var PI = Math.PI;
 
 
-				var getRad=function(d){
+				var getRad = function(d) {
 					return d * PI / 180.0;
 				}
-				
-			    var f = getRad((lat1 + lat2)/2);
-			    var g = getRad((lat1 - lat2)/2);
-			    var l = getRad((lng1 - lng2)/2);
-			    
-			    var sg = Math.sin(g);
-			    var sl = Math.sin(l);
-			    var sf = Math.sin(f);
-			    
-			    var s,c,w,r,d,h1,h2;
-			    var a = EARTH_RADIUS;
-			    var fl = 1/298.257;
-			    
-			    sg = sg*sg;
-			    sl = sl*sl;
-			    sf = sf*sf;
-			    
-			    s = sg*(1-sl) + (1-sf)*sl;
-			    c = (1-sg)*(1-sl) + sf*sl;
-			    
-			    w = Math.atan(Math.sqrt(s/c));
-			    r = Math.sqrt(s*c)/w;
-			    d = 2*w*a;
-			    h1 = (3*r -1)/2/c;
-			    h2 = (3*r +1)/2/s;
-			    
-			    return d*(1 + fl*(h1*sf*(1-sg) - h2*(1-sf)*sg));
+
+				var f = getRad((lat1 + lat2) / 2);
+				var g = getRad((lat1 - lat2) / 2);
+				var l = getRad((lng1 - lng2) / 2);
+
+				var sg = Math.sin(g);
+				var sl = Math.sin(l);
+				var sf = Math.sin(f);
+
+				var s, c, w, r, d, h1, h2;
+				var a = EARTH_RADIUS;
+				var fl = 1 / 298.257; 
+
+				sg = sg * sg;
+				sl = sl * sl;
+				sf = sf * sf;
+
+				s = sg * (1 - sl) + (1 - sf) * sl;
+				c = (1 - sg) * (1 - sl) + sf * sl;
+
+				w = Math.atan(Math.sqrt(s / c));
+				r = Math.sqrt(s * c) / w;
+				d = 2 * w * a;
+				h1 = (3 * r - 1) / 2 / c;
+				h2 = (3 * r + 1) / 2 / s;
+
+				return d * (1 + fl * (h1 * sf * (1 - sg) - h2 * (1 - sf) * sg));
 			},
-			markertap(e){	//用于告知组件按下的marker编号
+			markertap(e) { //用于告知组件按下的marker编号
 				var number;
-				for(var index in this.covers){
-					if(this.covers[index].id==e.detail.markerId){
-						number=index;
+				for (var index in this.covers) {
+					if (this.covers[index].id == e.detail.markerId) {
+						number = index;
 					}
 				}
-				
-				if(this.markerSelected==number){	//第二次点击再生效
-					this.$store.commit('setMarkerSelected',number);
+
+				if (this.markerSelected == number) { //第二次点击再生效
+					this.$store.commit('setMarkerSelected', number);
 					this.MoveLocation(this.covers[number].latitude, this.covers[number].longitude);
 				}
-				this.markerSelected=number;
+				this.markerSelected = number;
 			}
 		},
 		watch: {
-			'$store.state.destination'() {//监听destination变化 变化就在地图上加入标记点并且移动到该位置
-				if(this.$store.state.destination!=null){//按下locationbutton重置回到自己位置，destination置为空
-					this.$store.state.relocate=false;
+			'$store.state.destination'() { //监听destination变化 变化就在地图上加入标记点并且移动到该位置
+				if (this.$store.state.destination != null) { //按下locationbutton重置回到自己位置，destination置为空
+					this.$store.state.relocate = false;
 					wx.stopLocationUpdate();
 					var title = this.$store.state.destination.title;
 					var latitude = this.$store.state.destination.location.lat;
@@ -234,14 +240,14 @@
 					this.MoveLocation(latitude, longitude);
 				};
 			},
-			'$store.state.relocate'(){
-				if(this.$store.state.relocate==true)
+			'$store.state.relocate'() {
+				if (this.$store.state.relocate == true)
 					this.openLocation();
 			},
-			'$store.state.orderSelected'(){ //当选中一个order后在地图上移动到该标记点
-				var latitude=this.covers[this.$store.state.orderSelected].latitude;
-				var longitude=this.covers[this.$store.state.orderSelected].longitude;
-				this.MoveLocation(latitude,longitude);
+			'$store.state.orderSelected'() { //当选中一个order后在地图上移动到该标记点
+				var latitude = this.covers[this.$store.state.orderSelected].latitude;
+				var longitude = this.covers[this.$store.state.orderSelected].longitude;
+				this.MoveLocation(latitude, longitude);
 			},
 			'$store.state.locationres'() {
 				let res = this.$store.state.locationres;
