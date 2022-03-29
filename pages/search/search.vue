9aa9fe70cd9f0124e50f9a760f6298535da71ee2
Saint.JR <1911619172@qq.com>
2022-03-29 21:28:50 +0800

    v 1.2.8 全面优化搜索页面 更改了历史记录和常用地点之间的切换动画 新增了搜索时的关键字提示 新增了搜索时地点的距离

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -33,51 +33,67 @@
 				<view class="clear" @tap="clear">清空历史记录</view>
 			</view>
 			
-			<swiper :style="{'height': height+'px'}" @animationfinish="animationfinish($event)" @transition="transition($event)">
+			<swiper :style="{'height': (storageHeight+20)+'px'}" @animationfinish="animationfinish($event)" @transition="transition($event)">
 				<swiper-item>
-					<scroll-view
-						scroll-y="true"
-						style="margin-top: 20upx;"
-						:style="{'height': height+'px'}"
-					>
-						<view
-							class="storage"
-							v-for="(storage,index) in storages"
-							:key="index"
-							@tap="tapStorage(storage.title,storage.location)"
+						<scroll-view
+							scroll-y="true"
+							style="margin: 30upx;background-color: #FFFFFF;border-radius: 40upx;width:690upx"
+							:style="{'height': storageHeight+'px'}"
 						>
-							<view class="view5">
-								<image src="../../static/image/search.png" class="image3"></image>
-								<text class="text3">{{storage.title}}</text>
+							<view v-for="(storage,index) in storages" :key="index" style="display: flex;flex-direction: column;">
+								<view
+									class="storage"
+									@tap="tapStorage(storage.title,storage.location)"
+								>
+								
+									<view class="view5">
+										<image src="../../static/image/search.png" class="image3"></image>
+										<text class="text3">{{storage.title}}</text>
+									</view>
+									
+									<view class="view4" @tap.stop.prevent="del(index)">
+										<icon type="cancel" color="rgba(102,205,170,1)" style="margin-top: 5upx;"></icon>
+									</view>
+								</view>
+								<view class="view6">
+									<text class="text6">{{storage.category}}</text>
+								</view>
+								<view style="display: flex;justify-content: center;margin-top: 20upx;">
+									<view style="border-bottom: 1px solid rgba(0,0,0,0.1);width: 500upx;"></view>
+								</view>
+								
 							</view>
-							
-							<view class="view4" @tap.stop.prevent="del(index)">
-								<icon type="cancel" color="rgba(102,205,170,1)" style="margin-top: 10upx;"></icon>
-							</view>
-						</view>
-					</scroll-view>
+						
+						
+						</scroll-view>
 				</swiper-item>
 				<swiper-item>
 					<scroll-view
 						scroll-y="true"
-						style="margin-top: 20upx;"
-						:style="{'height': height+'px'}"
+						style="margin: 30upx;background-color: #FFFFFF;border-radius: 40upx;width:690upx"
+						:style="{'height': storageHeight+'px'}"
 					>
-						<view
-							class="storage"
-							v-for="(storage,index) in storages"
-							:key="index"
-							@tap="tapStorage(storage.title,storage.location)"
-						>
-							<view class="view5">
-								<image src="../../static/image/search.png" class="image3"></image>
-								<text class="text3">{{storage.title}}</text>
+						<view v-for="(storage,index) in storages" :key="index" style="display: flex;flex-direction: column;">
+							<view
+								class="storage"
+								@tap="tapStorage(storage.title,storage.location)"
+							>
+								<view class="view5">
+									<image src="../../static/image/search.png" class="image3"></image>
+									<text class="text3">{{storage.title}}</text>
+								</view>
+								
+								<view class="view4" @tap.stop.prevent="del(index)">
+									<icon type="cancel" color="rgba(102,205,170,1)" style="margin-top: 5upx;"></icon>
+								</view>
+							</view>
+							<view style="display: flex;justify-content: center;">
+								<view style="border-bottom: 1px solid rgba(0,0,0,0.1);width: 500upx;"></view>
 							</view>
 							
-							<view class="view4" @tap.stop.prevent="del(index)">
-								<icon type="cancel" color="rgba(102,205,170,1)" style="margin-top: 10upx;"></icon>
-							</view>
 						</view>
+					
+					
 					</scroll-view>
 				</swiper-item>
 			</swiper>
@@ -88,23 +104,34 @@
 		<scroll-view
 			v-if="isInput"
 			scroll-y="true"  
-			:style="{'height': height+'px'}"
+			style="margin: 30upx;background-color: #FFFFFF;border-radius: 40upx;width:690upx"
+			:style="{'height': suggestionHeight+'px'}"
 		>		
 			<view class="suggestion" 
 				
 				v-for="(suggestion,index) in suggestions" 
 				:key="index"
-				@tap="tap(suggestion.id,suggestion.title,suggestion.location)"
+				@tap="tap(suggestion.id,suggestion.title,suggestion.location,suggestion.category)"
 			>
-				<view class="view1">
-					<image class="image2" src="../../static/image/landmark.png"></image>
-					<text class="text1">{{suggestion.title}}</text>
+				<view style="display: flex;justify-content: space-between;">
+					<view class="view1">
+						<image class="image2" src="../../static/image/landmark.png"></image>
+						<rich-text :nodes="suggestion.strings"></rich-text>
+					</view>
+					<view style="position: absolute;right: 40upx;top: 5upx;display: flex;flex-direction: column;justify-content: center;">
+						<image src='../../static/image/distance.png' style="width: 80upx;height: 80upx;"></image>
+						<text style="font-size: 23upx;color: rgb(0,0,0,0.6);margin-top: 5upx;">{{suggestion.distance}}km</text>
+					</view>
 				</view>
+				
 				<view class="view2">
 					<text class="text2">{{suggestion.category}}</text>
 				</view>
 				<view class="view3">
-					<text>{{suggestion.address}}</text>
+					<text style="font-size: 23upx;color: rgb(0,0,0,0.6);">{{suggestion.address}}</text>
+				</view>
+				<view style="display: flex;justify-content: center;">
+					<view style="border-bottom: 1px solid rgba(0,0,0,0.1);width: 500upx;"></view>
 				</view>
 			</view>
 		</scroll-view>
@@ -120,7 +147,8 @@
 			return{
 				position:"",
 				suggestions:[],
-				height:0,
+				storageHeight:0,
+				suggestionHeight:0,
 				index:0,
 				storages:[],
 				isInput:false,
@@ -134,13 +162,16 @@
 		methods:{
 			request(){
 				this.isInput=true;
+				var url='https://apis.map.qq.com/ws/place/v1/suggestion?keyword='+this.position+'&location='
+					+this.$store.state.currentLocation.latitude+','+this.$store.state.currentLocation.longitude+'&address_format=short'
+					+'&key=ORFBZ-V73LX-N3Z4Y-Z3MR4-V35MJ-LNBFL';
 				uni.request({
-					url:'https://apis.map.qq.com/ws/place/v1/suggestion?keyword='+this.position+'&key=ORFBZ-V73LX-N3Z4Y-Z3MR4-V35MJ-LNBFL',
+					url:url,
 					method:'GET',
 					success: (res) => {
+						console.log(url)
 						if(res.data.status =='0'){
 							this.suggestions.splice(0);
-							
 							for(var index in res.data.data){
 								this.suggestions.push({
 									id:res.data.data[index].id,
@@ -148,18 +179,33 @@
 									address: res.data.data[index].address,
 									category: res.data.data[index].category,
 									location: res.data.data[index].location,
+									distance: (res.data.data[index]._distance/1000).toFixed(1),
+									strings: this.keyword(res.data.data[index].title,this.position),
 								});
-							}
+							};
 						} 
 					},
 				})
 			},
-			tap(id,title,location){
+			keyword(title,position){
+				if(title.includes(position)){
+					var html=(title.replace(
+						position, 
+						'<span style="color:rgb(102,205,170);">'+ position +'</span>'
+					));
+					html='<span class="text1">'+html+'</span>';
+					return html;
+				}else{
+					return title;
+				}
+			},
+			tap(id,title,location,category){
 				var keys=uni.getStorageInfoSync().keys;
 				uni.setStorageSync(id,
 					{
 						title:title,
-						location:location
+						location:location,
+						category:category
 					}
 				);
 				if(keys.length>=10){
@@ -218,9 +264,7 @@
 				}
 			},
 			animationfinish(e){
-				console.log(e)
 				this.currentPage=e.detail.current;
-				console.log(this.currentPage)
 			},
 			transition(e){
 				
@@ -233,14 +277,14 @@
 						})
 						
 						this.modelWidth=uni.upx2px(120+280*percent*2);
-						this.modelHeight=uni.upx2px(15-8*percent*2);
+						// this.modelHeight=uni.upx2px(15-8*percent*2);
 					}else{
 						this.$nextTick(function(){
 							this.justifyContent="flex-end";
 						})
 						
 						this.modelWidth=uni.upx2px(400-280*(percent-0.5)*2);
-						this.modelHeight=uni.upx2px(7+8*(percent-0.5)*2);
+						// this.modelHeight=uni.upx2px(7+8*(percent-0.5)*2);
 						// console.log(this.modelWidth)
 					}
 				}else{
@@ -250,14 +294,14 @@
 						})
 						
 						this.modelWidth=uni.upx2px(120+280*percent*2);
-						this.modelHeight=uni.upx2px(15-8*percent*2);
+						// this.modelHeight=uni.upx2px(15-8*percent*2);
 					}else{
 						this.$nextTick(function(){
 							this.justifyContent="flex-start";
 						})
 						
 						this.modelWidth=uni.upx2px(400-280*(percent-0.5)*2);
-						this.modelHeight=uni.upx2px(7+8*(percent-0.5)*2);
+						// this.modelHeight=uni.upx2px(7+8*(percent-0.5)*2);
 						// 
 					}
 				}
@@ -274,7 +318,8 @@
 				}
 			}
 			this.windowWidth=uni.getSystemInfoSync().windowWidth;
-			this.height=uni.getSystemInfoSync().windowHeight*0.85;
+			this.storageHeight=uni.getSystemInfoSync().windowHeight*0.8;
+			this.suggestionHeight=uni.getSystemInfoSync().windowHeight*0.85;
 			this.modelHeight=uni.upx2px(15);
 			this.modelWidth=uni.upx2px(120);
 			
@@ -296,7 +341,9 @@
 		position:relative;
 		height: 50upx;
 		width: 50upx;
-		top:15upx;
+		/* margin-right: 10upx;
+		margin-bottom: 10upx; */
+		bottom:7upx;
 		right:10upx;
 		
 	}
@@ -329,7 +376,7 @@
 		font-size: 32upx;
 		letter-spacing: 1upx;
 		border-radius: 20upx;
-		border: 3px solid rgba(102,205,170,0.5);
+		border: 3px outset rgba(102,205,170,0.5);
 	}
 	
 	.searchtext{
@@ -391,26 +438,30 @@
 	}
 	
 	.storage{
-		background-color: rgb(255,255,255);
-		margin: 30upx;
+		margin-top: 10upx;
+		/* margin-bottom: 10upx; */
 		/* margin-left: 30upx;
 		margin-right: 30upx; */
 		padding: 15upx;
 		padding-left: 20upx;
+		padding-bottom: 5upx;
 		border-radius: 10upx;
 		display: flex;
 		justify-content: space-between;
+		overflow: hidden;
+		/* border: 2px solid red; */
 	}
 	
 	.view5{
 		position: relative;
 		display: flex;
-		
+		/* border: 2px solid red; */
 	}
 	
 	.text3{
 		margin: 15upx;
 		margin-top: 18upx;
+		margin-bottom: 0upx;
 		white-space: nowrap;
 		overflow: hidden;
 		text-overflow: ellipsis;
@@ -418,6 +469,8 @@
 		font-size: 29upx;
 		font-weight: 700;
 		letter-spacing: 1upx;
+		/* border: 2px solid red; */
+		/* border-bottom: 2px solid rgba(0,0,0,0.2); */
 	}
 	
 	.view4{
@@ -427,17 +480,21 @@
 	}
 	
 	.suggestion{
-		background-color: rgb(255,255,255);
-		margin: 20upx;
-		margin-left: 30upx;
-		margin-right: 30upx;
+		/* background-color: rgb(255,255,255); */
+		margin-bottom: 10upx;
+		/* margin-left: 30upx;
+		margin-right: 30upx; */
 		border-radius: 10upx;
 		display: flex;
 		flex-direction: column;
+		justify-content: center;
+		position: relative;
 	}
 	
 	.view1{
 		margin: 20upx;
+		/* margin-top: 20upx; */
+		margin-bottom: 10upx;
 		margin-left: 30upx;
 		font-size: 30upx;
 		font-weight: 700;
@@ -445,6 +502,7 @@
 		white-space: nowrap;
 		overflow: hidden;
 		text-overflow: ellipsis;
+		display: flex;
 	}
 	
 	.text2{
@@ -457,6 +515,17 @@
 	
 	.view3{
 		margin: 20upx;
+		/* margin-top: 30upx; */
 		margin-left: 82upx;
+	}
+	
+	.text6{
+		background-color:rgba(102,205,170,0.2) ;
+		color: rgba(102,205,170,1);
+		margin-left: 100upx;
+		margin-bottom: 20upx;
+		padding: 10upx;
+		font-size: 20upx;
+		
 	}
 </style>
